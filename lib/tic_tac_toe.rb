8143a44585c1e20d2b588115a4ce0c691c8f8a89
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-20 21:43:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,7 +61,7 @@ def current_player
   turn_count % 2 == 0 ? "X" : "O"
 end
 
-def won?(board)
+def won?
   WIN_COMBINATIONS.detect do |combo|
     @board[combo[0]] == @board[combo[1]] &&
     @board[combo[1]] == @board[combo[2]] &&
