e323868f71bf5decf9b618bca30cd6553d4d2ebf
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-20 21:42:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,7 +65,7 @@ def won?(board)
   WIN_COMBINATIONS.detect do |combo|
     @board[combo[0]] == @board[combo[1]] &&
     @board[combo[1]] == @board[combo[2]] &&
-    position_taken?(combo[0])
+    position_taken?(0)
   end
 end
 
