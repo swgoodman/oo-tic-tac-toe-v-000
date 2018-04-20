3aaebd0d8e1125eac3d39bed5471cc48fa06506a
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-20 21:37:29 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,7 +41,7 @@ def valid_move?(index)
   index.between?(0,8) && !position_taken?(index)
 end
 
-def turn(board)
+def turn
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(user_input)
