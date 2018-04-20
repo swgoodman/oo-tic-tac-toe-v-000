427ce860a1c896a244a7a819796de0a41c40fcad
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-20 21:38:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,7 +43,7 @@ end
 
 def turn
   puts "Please enter 1-9:"
-  input = gets.strip
+  user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(index)
     move(index, current_player)
