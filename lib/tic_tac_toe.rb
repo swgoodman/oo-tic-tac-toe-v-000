72d7a747dd8cf24b0fa8dcc67cbbd9d1096bd3fc
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-20 21:38:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,9 +47,9 @@ def turn
   index = input_to_index(user_input)
   if valid_move?(index)
     move(index, current_player)
-    display_board(board)
+    @board
   else
-    turn(board)
+    turn
   end
 end
 
